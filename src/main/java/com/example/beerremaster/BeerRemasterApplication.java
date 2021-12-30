d5f38431addf57713b5d35e12041639714d14cf3
Olex1313 <aa1limonov@gmail.com>
2021-12-30 17:36:29 +0400

    :tada: Start new project

---
+++ src/main/java/com/example/beerremaster/BeerRemasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.beerremaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerRemasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerRemasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/beerremaster/BeerRemasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beerremaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerRemasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
